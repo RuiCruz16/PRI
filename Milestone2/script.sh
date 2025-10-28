5e115a4ff730f1a7991e9ee5bbc77fd9143a1f3f
mguerrinha <mlguerrinha@gmail.com>
2025-10-28 01:31:51 +0000

    schema definition

--- Milestone2/script.sh
+++ Milestone2/script.sh
@@ -3,6 +3,11 @@
 # This script expects a container started with the following command:
 # docker run -p 8984:8983 --name pri_proj -v ${PWD}:/data -d solr:9 solr-precreate diseases
 
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary "@./schema.json" \
+    http://localhost:8984/solr/diseases/schema
+
 # Populate collection using mapped path inside container
 # The -c flag specifies the collection name
 # CSV files are automatically detected and processed
