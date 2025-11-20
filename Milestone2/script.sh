869e9306cc5ffcee2561b86b4be09908ccaf1f7b
mguerrinha <mlguerrinha@gmail.com>
2025-11-20 15:35:05 +0000

    some changes

--- Milestone2/script.sh
+++ Milestone2/script.sh
@@ -4,8 +4,8 @@
 # docker run -p 8984:8983 --name pri_proj -v ${PWD}:/data -d solr:9 solr-precreate diseases
 
 # If having problems with collection just delete it and recreate:
-# docker exec -it pri_proj solr delete -c diseases
-# docker exec -it pri_proj solr create -c diseases
+docker exec -it pri_proj solr delete -c diseases
+docker exec -it pri_proj solr create -c diseases
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
